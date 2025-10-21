c072193e543ada4c3cf4b74d095b49295c74e8aa
Adrian <169140637+personagenerica@users.noreply.github.com>
2025-10-21 12:46:34 +0200

    Update AdminRepository.java

--- BACKEND/src/main/java/com/gimnasio/repository/AdminRepository.java
+++ BACKEND/src/main/java/com/gimnasio/repository/AdminRepository.java
@@ -3,7 +3,9 @@ package com.gimnasio.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.gimnasio.entity.Admin;
+
 @Repository
-public interface AdminRepository extends JpaRepository<AdminRepository,Integer>{
+public interface AdminRepository extends JpaRepository<Admin,Integer>{
 
 }
