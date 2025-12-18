26159e82f16bfdeaa27da909e028606d6a52e604
Adrian <169140637+personagenerica@users.noreply.github.com>
2025-12-18 10:15:11 +0100

    Update GuardarProducto.java

--- BACKEND/src/main/java/com/gimnasio/GuardarProducto.java
+++ BACKEND/src/main/java/com/gimnasio/GuardarProducto.java
@@ -11,7 +11,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+//Crear controlador
 public class GuardarProducto {
 
     public static void main(String[] args) {
